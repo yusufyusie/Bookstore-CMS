8e51e90e1f37b4cfe74cb7658a7fad79434056f1
yusufyusie <yesuf023@gmail.com>
2023-08-25 21:43:40 +0300

    Update book component to add required data

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 const Book = (props)=>{
     const {title, author} = props;
@@ -10,5 +11,10 @@ const Book = (props)=>{
         </li>
     )
 };
+
+Book.propTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  };
 
 export default Book;
