de7c510e97c188147f2937a4b9daba837dd26e4d
yusufyusie <yesuf023@gmail.com>
2023-08-25 22:13:44 +0300

    Add navbar component

---
+++ src/components/Categories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Categories = ()=>{
+    return (
+        <button type="button"> check status </button>
+    )
+};
+
+export default Categories;
--- src/components/bookList.jsx
+++ src/components/bookList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Book from "./Book"
+import Book from "./Book";
 import AddBook from "./addBookForm";
 
 const BookList = ()=>{
