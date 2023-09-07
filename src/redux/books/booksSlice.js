f5841e9ec2c43d4e32484d11cd5dede156f3e655
yusufyusie <yesuf023@gmail.com>
2023-09-07 23:33:07 +0300

    Create an initialState variable

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,8 +1,28 @@
 const ADD_BOOK = 'Bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'Bookstore/books/REMOVE_BOOK';
 
-const initialState = [];
-
+const initialState = {
+  booksItem: [
+    {
+      itemId: 'item1',
+      category: 'Action',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+    },
+    {
+      itemId: 'item2',
+      category: 'Fiction',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+    },
+    {
+      itemId: 'item3',
+      category: 'Nonfiction',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+    },
+  ],
+};
 // Actions
 
 export const addBook = (book) => ({
