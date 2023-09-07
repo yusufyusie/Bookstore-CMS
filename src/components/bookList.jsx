14b2172bb605d561ff13bee1c6de6399f674989c
yusufyusie <yesuf023@gmail.com>
2023-09-07 23:48:25 +0300

    Add Display your books

--- src/components/bookList.jsx
+++ src/components/bookList.jsx
@@ -1,37 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 import Book from './Book';
 
 const BookList = () => {
-  const bookCollection = [
-    {
-      id: 1,
-      title: 'book 1',
-      author: 'Author 1',
-    },
-
-    {
-      id: 2,
-      title: 'book 2',
-      author: 'Author 2',
-    },
-
-    {
-      id: 3,
-      title: 'book 3',
-      author: 'Author 3',
-    },
-  ];
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
   return (
     <div className="book-card">
       <ul className="book">
-        {bookCollection.map((book) => (
-          // eslint-disable-next-line react/no-unknown-property
-          <li className="book-list" Book key={book.id}>
+        {books.map((book) => (
+          <li className="book-list" key={book.id}>
             <Book title={book.title} author={book.author} />
             <div>
               <button type="button">Comments</button>
               <span>|</span>
-              <button type="button">Remove</button>
+              <button type="button" onClick={() => dispatch(removeBook(book.id))}>Remove</button>
               <span>|</span>
               <button type="button">Edit</button>
             </div>
