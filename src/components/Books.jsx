6490fb510d87ba62e9f5258f4cdb0def942c1a28
yusufyusie <yesuf023@gmail.com>
2023-09-09 09:09:30 +0300

    Update books component to add getbook dispach

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import BookList from './bookList';
 import AddBook from './addBookForm';
+import getBooks from '../redux/books/booksSlice';
 
-const Books = () => (
-  <section className="bookstore-cms">
-    <BookList />
-    <AddBook />
-  </section>
-);
+const Books = () => {
+  const dispatch = useDispatch();
+  dispatch(getBooks());
+  return (
+    <section className="bookstore-cms">
+      <BookList />
+      <AddBook />
+    </section>
+  );
+};
 
 export default Books;
--- src/redux/booksAPI.js
+++ src/redux/booksAPI.js
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { FETCH_BOOKS, fetchBooks, ADD_BOOK, addBook, REMOVE_BOOK, removeBook } from './books/booksSlice';
+import {
+  FETCH_BOOKS, fetchBooks, ADD_BOOK, addBook, REMOVE_BOOK, removeBook,
+} from './books/booksSlice';
 
 const APP_ID = 'doVfOZKYHMqPNDJVFHKX';
 const BASE_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}`;
@@ -15,31 +17,31 @@ const getBooks = createAsyncThunk(FETCH_BOOKS, async (_, thunkAPI) => {
 });
 
 const postBook = createAsyncThunk(ADD_BOOK, async (book, thunkAPI) => {
-    const response = await fetch(BOOK_URL, {
-      method: 'POST',
-      body: JSON.stringify(book),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const responseText = await response.text();
-    thunkAPI.dispatch(addBook(book));
-    return responseText;
+  const response = await fetch(BOOK_URL, {
+    method: 'POST',
+    body: JSON.stringify(book),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
+  const responseText = await response.text();
+  thunkAPI.dispatch(addBook(book));
+  return responseText;
+});
 
-  const deleteBook = createAsyncThunk(REMOVE_BOOK, async (bookId, thunkAPI) => {
-    const response = await fetch(`${BOOK_URL}/${bookId}`, {
-      method: 'DELETE',
-      body: JSON.stringify({
-        item_id: bookId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const responseText = await response.text();
-    thunkAPI.dispatch(removeBook(bookId));
-    return responseText;
+const deleteBook = createAsyncThunk(REMOVE_BOOK, async (bookId, thunkAPI) => {
+  const response = await fetch(`${BOOK_URL}/${bookId}`, {
+    method: 'DELETE',
+    body: JSON.stringify({
+      item_id: bookId,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
+  const responseText = await response.text();
+  thunkAPI.dispatch(removeBook(bookId));
+  return responseText;
+});
 
-export default {getBooks,postBook, deleteBook};
+export default { getBooks, postBook, deleteBook };
